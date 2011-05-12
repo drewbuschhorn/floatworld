b74c02eb6cb70c4004b653cdb54280c5e4b38dfe
Drew Buschhorn <drewbuschhorn@gmail.com>
2011-05-11 23:01:35 -0400

    Changed webserver from demo over to full plugin archetectured pion server.

--- gui/main.cpp
+++ gui/main.cpp
@@ -5,11 +5,15 @@
 
 //Start HelloServer Includes
 #include <iostream>
+#include <vector>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <boost/thread.hpp> 
-#include <pion/net/TCPServer.hpp>
+#include <pion/PionPlugin.hpp>
+#include <pion/net/WebServer.hpp>
 #include "ShutdownManager.hpp"
+
+#include "../src/HelloService.hpp"
 //End HelloServer Includes
 
 #include <sstream>
@@ -20,23 +24,8 @@ using namespace pion;
 using namespace pion::net;
 //End HelloServer namespaces
 
-//Start HelloServer classes
-/// simple TCP server that just sends "Hello there!" to each connection
-class HelloServer : public TCPServer {
-public:
-	HelloServer(const unsigned int tcp_port) : TCPServer(tcp_port) {}
-	virtual ~HelloServer() {}
-	virtual void handleConnection(TCPConnectionPtr& tcp_conn)
-	{
-		World* world = World::Instance();
-		std::ostringstream out; 
-		out << "<?xml version=\"1.0\" encoding=\"UTF-8\"?><timestamp>"<< world->getTimestep() <<"</timestamp>";
-		tcp_conn->setLifecycle(TCPConnection::LIFECYCLE_CLOSE);	// make sure it will get closed
-		tcp_conn->async_write(boost::asio::buffer(out.str()),
-							  boost::bind(&TCPConnection::finish, tcp_conn));
-	}
-};
-//End HelloServer classes
+
+
 
 void func(){
 //Start HelloServer server startup
@@ -46,21 +35,28 @@ void func(){
 	#else
 		signal(SIGINT, handle_signal);
 	#endif
-
-	// initialize log system (use simple configuration)
-	PionLogger main_log(PION_GET_LOGGER("PionHelloServer"));
-	PionLogger pion_log(PION_GET_LOGGER("pion"));
-	PION_LOG_SETLEVEL_INFO(main_log);
-	PION_LOG_SETLEVEL_INFO(pion_log);
-	PION_LOG_CONFIG_BASIC;
 	
 	try {
 		// create a new server to handle the Hello TCP protocol
-		TCPServerPtr hello_server(new HelloServer(8080));
-		hello_server->start();
+		// create a server for HTTP & add the Hello Service
+		WebServer  web_server(8080);
+
+		// add the Pion plug-ins installation directory to our path
+		try { PionPlugin::addPluginDirectory("../src/"); }
+		catch (PionPlugin::DirectoryNotFoundException&) {
+		//	PION_LOG_WARN(main_log, "Default plug-ins directory does not exist: "
+		//		<< PION_PLUGINS_DIRECTORY);
+		//	cout << "Default plug-ins directory does not exist: "
+		//		<< PION_PLUGINS_DIRECTORY;
+		}
+
+		pion::net::WebService* service = new pion::plugins::HelloService(); 
+		web_server.addService("/hello", service);
+		web_server.start();
 		main_shutdown_manager.wait();
+		delete service;
 	} catch (std::exception& e) {
-		PION_LOG_FATAL(main_log, e.what());
+		//PION_LOG_FATAL(main_log, e.what());
 		cout << e.what() << endl;
 	}
 //End HelloServer server startup
